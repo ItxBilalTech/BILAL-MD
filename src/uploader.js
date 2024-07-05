520168ae97dd88de681b6d3d5f51c1afcc152e75
Mr Bilal <156580171+Bilalking001@users.noreply.github.com>
2024-07-05 13:35:25 +0100

    Update uploader.js

--- src/uploader.js
+++ src/uploader.js
@@ -78,7 +78,7 @@ export const webp2mp4File = async (path) => {
       const result = 'https:' + $2('div#output > p.outfile > video > source').attr('src');
       resolve({
         status: true,
-        message: "Created By Ethix-MD",
+        message: "Created By BILAL",
         result: result
       });
     } catch (err) {
