372dbc3d42681aa31170af5702c5157718e31b83
Mr Bilal <156580171+Bilalking001@users.noreply.github.com>
2024-07-05 13:51:13 +0100

    Update video.js

--- src/plugin/video.js
+++ src/plugin/video.js
@@ -35,7 +35,7 @@ const video = async (m, Matrix) => {
 
             const videoInfo = await yts({ videoId: ytdl.getURLVideoID(text) });
     
-            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿' }, { quoted: m });
+            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by KING-BILAL-MD�' }, { quoted: m });
             await m.React("✅");
           } catch (err) {
             console.error('Error sending video:', err);
@@ -67,7 +67,7 @@ const video = async (m, Matrix) => {
           try {
             const finalVideoBuffer = Buffer.concat(videoBuffer);
           
-            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by 𝞢𝙏𝞖𝞘𝞦-𝞛𝘿' }, { quoted: m });
+            await Matrix.sendMessage(m.from, { video: finalVideoBuffer, mimetype: 'video/mp4', caption: '> © Powered by KING-BILAL-MD�' }, { quoted: m });
             await m.React("✅");
           } catch (err) {
             console.error('Error sending video:', err);
