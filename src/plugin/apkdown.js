631421aa79c716e3e56f37a8f8114d9d8c8f4a31
Mr Bilal <156580171+Bilalking001@users.noreply.github.com>
2024-07-05 20:59:16 +0500

    KING BILAL MD

--- src/plugin/apkdown.js
+++ src/plugin/apkdown.js
@@ -73,7 +73,7 @@ const searchAPK = async (m, Matrix) => {
                 text: "© Powered By KING-BILAL-MD"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
-                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/fbbe1744668b44637c21a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/05e1fde9eea24516fd6e0.jpg` } }, { upload: Matrix.waUploadToServer })),
                 title: ``,
                 gifPlayback: true,
                 subtitle: "",
