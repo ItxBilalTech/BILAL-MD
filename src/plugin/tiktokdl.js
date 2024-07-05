4e44a0a1acd1705456b340c96fc4b35ed3498bed
Mr Bilal <156580171+Bilalking001@users.noreply.github.com>
2024-07-05 14:16:26 +0100

    Update tiktokdl.js

--- src/plugin/tiktokdl.js
+++ src/plugin/tiktokdl.js
@@ -82,7 +82,7 @@ const tiktokCommand = async (m, Matrix) => {
                 text: "© Powered By BILAL-md�"
               }),
               header: proto.Message.InteractiveMessage.Header.create({
-                 ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/fbbe1744668b44637c21a.jpg` } }, { upload: Matrix.waUploadToServer })),
+                 ...(await prepareWAMessageMedia({ image: { url: `https://telegra.ph/file/05e1fde9eea24516fd6e0.jpg` } }, { upload: Matrix.waUploadToServer })),
                 title: "",
                 gifPlayback: true,
                 subtitle: "",
