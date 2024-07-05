23d10dd443b023b9db29c319a363cd04b95c6f1f
Mr Bilal <156580171+Bilalking001@users.noreply.github.com>
2024-07-05 21:34:44 +0500

    👑 KING-BILAL-MD 👑

--- src/plugin/fbdown.js
+++ src/plugin/fbdown.js
@@ -27,7 +27,7 @@ const facebookCommand = async (m, Matrix) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const validCommands = ['facebook', 'fb', 'fbdl'];
+  const validCommands = ['facebook', 'fb', 'fbdl','fc'];
 
   if (validCommands.includes(cmd)) {
     if (!text) {
